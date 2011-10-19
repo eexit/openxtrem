728f87bac4fef1552819f2f43a0e81a9d9f4b8a6
Joris Berthelot <admin@eexit.net>
2011-10-19 11:19:08 +0200

    Removed old code in application

--- app/app.php
+++ app/app.php
@@ -1,17 +1,4 @@
 <?php
-/*
-namespace Sysd\Validator;
-
-require_once __DIR__ . '/../src/Validator.php';
-
-$validator = new Validator($doc = __DIR__ . '/../var/patient.xml');
-$result = $validator->isValid($xsd = __DIR__ . '/../xsd/msgEvenementsPatients1053.xsd');
-
-if ($result) {
-    echo sprintf('%s%s was successfully validated against %s!%s', PHP_EOL, basename($doc), basename($xsd), PHP_EOL);
-}
-*/
-
 require_once __DIR__ . '/Loader.php';
 $client = new Icone\Sysd\Soap\Client\Cli\Client();
 $client->run();
